ba207fa3dff64d0f5fe1dfabae46371d85f43fbd
nightdentist <67285412+nightdentist@users.noreply.github.com>
2024-11-01 23:53:32 +0300

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -1,11 +1,29 @@
 import ptbot
 import os
 
+from pytimeparse import parse
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TG_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TG_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+def reply(chat_id, text):
+    bot.create_countdown(parse(text), notify_progress, chat_id=chat_id)
+    bot.create_timer(parse(text), answer, chat_id=chat_id)
+
+
+def notify_progress(secs_left, chat_id):
+    bot.send_message(chat_id, "Осталось секунд: {}".format(secs_left))
+
+
+def answer(chat_id):
+    bot.send_message(chat_id, "Время вышло!")
+
+
 bot = ptbot.Bot(TG_TOKEN)
-bot.send_message(TG_CHAT_ID, "Бот запущен")
+bot.reply_on_message(reply)
+bot.run_bot()
